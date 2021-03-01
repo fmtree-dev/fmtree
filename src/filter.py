3e80c79d8669adfe0bdd07b9cc75495457a403f7
Huakun Shen <huakun.shen@huakunshen.com>
2021-02-28 22:05:12 -0500

    Backup, update Filter, removed unused code

--- src/filter.py
+++ src/filter.py
@@ -11,33 +11,8 @@ class Filter(ABC):
     Path filter abstract class
     """
 
-    def __init__(self, root: pathlib2.Path = None,
-                 whitelist: Union[List[Union[str, pathlib2.Path]], None] = None,
-                 blacklist: Union[List[Union[str, pathlib2.Path]], None] = None):
-        """
-        allow and ignore list must be lists of absolute paths
-        if root is specified, then allow and ignore are both relative paths
-        else allow and ignore are absolute paths
-        :param root: root path
-        :param whitelist: List of allowed paths (either absolute or relative paths)
-        :param blacklist: List of ignored paths (either absolute or relative paths)
-        """
-        self.root = root
-        self.whitelist = whitelist if whitelist else []
-        self.blacklist = blacklist if blacklist else []
-        self._update()
-
-    def _update(self):
-        if self.root:
-            self.whitelist = [(self.root / path).resolve().absolute() for path in self.whitelist]
-            self.blacklist = [(self.root / path).resolve().absolute() for path in self.blacklist]
-
-    def set_root(self, root: pathlib2.Path):
-        self.root = root
-        # self.
-
     @abstractmethod
-    def __call__(self, path: pathlib2.Path):
+    def filter(self, path: pathlib2.Path) -> bool:
         """
         taking a path and decide whether it agrees with the filter
         :param path: pathlib2.Path: file path
@@ -45,13 +20,21 @@ class Filter(ABC):
         """
         raise NotImplementedError
 
+    def __call__(self, path: pathlib2.Path) -> bool:
+        """
+        taking a path and decide whether it agrees with the filter
+        :param path: pathlib2.Path: file path
+        :return: bool: whether agree with the filter
+        """
+        return self.filter(path)
+
 
 class MarkdownFilter(Filter):
     """
     A filter that keeps only markdown files and intermediate directories (non-files)
     """
 
-    def __call__(self, path: pathlib2.Path):
+    def filter(self, path: pathlib2.Path) -> bool:
         """
         Take a path and decide whether it's a markdown file
         :param path: pathlib2.Path: file path
@@ -72,7 +55,7 @@ class ExtensionFilter(Filter):
         """
         self._extensions = extensions
 
-    def __call__(self, path: pathlib2.Path):
+    def filter(self, path: pathlib2.Path) -> bool:
         """
         Decide if the given path has one of the allowed extensions (self._extensions)
         :param path: a pathlib2.Path file path
@@ -94,7 +77,7 @@ class RegexFilter(Filter):
         """
         self._pattern = re.compile(regex_pattern)
 
-    def __call__(self, path: pathlib2.Path):
+    def filter(self, path: pathlib2.Path) -> bool:
         """
         Take a path and decide whether it matches the regular expression self._pattern
         :param path: pathlib2.Path: file path
@@ -103,7 +86,7 @@ class RegexFilter(Filter):
         return self._pattern.match(str(path)) is not None
 
 
-def markdown_filter(path: pathlib2.Path):
+def markdown_filter(path: pathlib2.Path) -> bool:
     """
     A function form markdown file filter
     :param path: a pathlib2.Path path
