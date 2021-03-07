e6394dc926fd6be7751e028664f3375c95d2dd33
Huakun Shen <shenhuakun@outlook.com>
2021-03-06 23:39:13 -0500

    Merge remote-tracking branch 'origin/develop' into develop

--- fmtree/scraper.py
+++ fmtree/scraper.py
@@ -7,26 +7,42 @@ import pathlib2
 
 class BaseScraper(ABC):
     def __init__(self, path: pathlib2.Path, scrape_now: bool = False, filters: Iterable[BaseFileFilter] = None):
+        """Base Scraper Initializer
+
+        :param path: path to scrape
+        :type path: pathlib2.Path
+        :param scrape_now: whether start scraping when the object is created, defaults to False
+        :type scrape_now: bool, optional
+        :param filters: list of filters, defaults to None
+        :type filters: Iterable[BaseFileFilter], optional
+        """
         self.root = path.resolve().absolute()
         self.history = set()
-        self.tree = self.scrape(self.root, 0)[0] if scrape_now else None
         self.filters = filters if filters else []
         for filter_ in self.filters:
             filter_.set_root_path(self.root)
+        self.tree = self.scrape(self.root, 0)[0] if scrape_now else None
 
     @abstractmethod
     def scrape(self, path: pathlib2.Path, depth: int) -> Tuple[FileNode, bool]:
         raise NotImplementedError
 
     def add_filter(self, filter_: BaseFileFilter) -> None:
+        """Add a filter object to this Scraper
+
+        :param filter_: filter object
+        :type filter_: BaseFileFilter
+        """
         filter_.set_root_path(self.root)
         self.filters.append(filter_)
 
     def run(self, inplace: bool = True) -> FileNode:
-        """
-        scrape the given path and form a tree structure
-        :param inplace: bool: set tree inplace
+        """scrape the given path and form a tree structure
+
+        :param inplace: set tree inplace, defaults to True
+        :type inplace: bool, optional
         :return: the scraped tree of file nodes
+        :rtype: FileNode
         """
         self.history = set()
         tree, found_any = self.scrape(self.root, 0)
@@ -37,6 +53,7 @@ class BaseScraper(ABC):
     def get_tree(self) -> FileNode:
         """
         :return: root node of the scraped file tree
+        :rtype: FileNode
         """
         return self.tree
 
@@ -55,7 +72,8 @@ class Scraper(BaseScraper):
         :param filters: filters for filtering out unwanted files
         :param scrape_now: start scraping right after initialization
         """
-        super(Scraper, self).__init__(path, scrape_now=scrape_now, filters=filters)
+        super(Scraper, self).__init__(
+            path, scrape_now=scrape_now, filters=filters)
         self._keep_empty_dir = keep_empty_dir
         self.depth_limit = depth
         if not self.root.exists():
