9e51506ad481548b9bbead40b854987404f93b4f
Huakun Shen <33727687+HuakunShen@users.noreply.github.com>
2021-04-12 19:46:13 -0400

    Improve visualizer.image_dir (#7)
    * Remove indent for json in visualizer/image_dir
    
    * Improve visualizer.image_dir by adding custom image width and detailed view options.

--- fmtree/core/constants.py
+++ fmtree/core/constants.py
@@ -1,1 +1,2 @@
-HTML_IMAGE_EXTENSIONS = [".apng", ".avif", ".gif", ".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png", ".svg", ".webp"]
+HTML_IMAGE_EXTENSIONS = [".apng", ".avif", ".gif", ".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png", ".svg", ".webp",
+                         ".JPG"]
--- fmtree/visualizer/image_dir.py
+++ fmtree/visualizer/image_dir.py
@@ -9,6 +9,16 @@ from jinja2 import Environment, FileSystemLoader
 
 current_directory = pathlib2.Path(__file__).parent.absolute()
 
+bootstrap_css_cdn = """
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+"""
+bootstrap_js_cdn = """
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+"""
+jquery_cdn = """
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+"""
+show_all_js = "$('details').attr('open', true);"
 
 def main(args):
     print(args)
@@ -19,18 +29,22 @@ def main(args):
     formatter = TreeCommandFormatter(tree)
     if not args['silent']:
         formatter.to_stream(sys.stdout)
-    json_content = tree.to_json(indent=2)
+    json_content = tree.to_json(indent=None)
     file_loader = FileSystemLoader(str(current_directory / 'template'))
     env = Environment(loader=file_loader)
     template = env.get_template('index.html')
-    with open(str(current_directory / 'template' / 'assets' / 'bootstrap.min.css'), 'r') as f:
-        bootstrap_min_css = f.read()
-    with open(str(current_directory / 'template' / 'assets' / 'bootstrap.bundle.min.js'), 'r') as f:
-        bootstrap_min_js = f.read()
-    with open(str(current_directory / 'template' / 'assets' / 'jquery-3.6.0.min.js'), 'r') as f:
-        jquery_min_js = f.read()
-    output = template.render(data=json_content, bootstrap_css=bootstrap_min_css, bootstrap_js=bootstrap_min_js,
-                             jquery_js=jquery_min_js)
+    if args['cdn']:
+        bootstrap_css, bootstrap_js, jquery_js = bootstrap_css_cdn, bootstrap_js_cdn, jquery_cdn
+    else:
+        with open(str(current_directory / 'template' / 'assets' / 'bootstrap.min.css'), 'r') as f:
+            bootstrap_css = "<style>" + f.read() + "</style>"
+        with open(str(current_directory / 'template' / 'assets' / 'bootstrap.bundle.min.js'), 'r') as f:
+            bootstrap_js = "<script>" + f.read() + "</script>"
+        with open(str(current_directory / 'template' / 'assets' / 'jquery-3.6.0.min.js'), 'r') as f:
+            jquery_js = "<script>" + f.read() + "</script>"
+    show_all = show_all_js if args['show_all'] else ""
+    output = template.render(data=json_content, bootstrap_css=bootstrap_css, bootstrap_js=bootstrap_js,
+                             jquery_js=jquery_js, show_all=show_all)
     with open(str(pathlib2.Path(args['input']) / 'fmtree-image-visualizer.html'), 'w') as f:
         f.write(output)
     print("finished")
@@ -40,5 +54,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser("Fmtree Visualizer Parser")
     parser.add_argument('input', help='input path')
     parser.add_argument('--silent', type=bool, default=False, help="whether to print out the directory")
+    parser.add_argument('-d', '--depth', type=int, default=10, help="Directory depth to parse")
+    parser.add_argument('--cdn', action='store_true',
+                        help="Use CDN for libraries, requires internet access, minimize html size")
+    parser.add_argument('--show_all', action='store_true', help="Show All Images By Default")
     args = parser.parse_args()
     main(dict(args.__dict__))
